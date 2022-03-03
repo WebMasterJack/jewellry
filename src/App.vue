94715a72ac5b9158e26349ccbc6b16832530abb2
s2d1ent <vornfrost@gmail.com>
2022-03-03 09:51:11 +0300

    jew rework v 2.0 started

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+  <router-view/>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
+import './assets/js/bootstrap.bundle.js'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import './assets/css/bootstrap.css';
+@import './assets/css/style.css';
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="header">
+        
+        <nav class="navbar navbar-expand-lg navbar-light w-75 container">
+  <div class="container-fluid ">
+      <div class="logo">Logo</div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNav">
+        
+      <ul class="navbar-nav d-flex justify-content-between w-100">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Главная</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Кольца</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Серьги</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Цепочки</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Браслеты</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Кресты</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Кулоны</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Подвески</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Ремонт</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Header',
+
+}
+</script>

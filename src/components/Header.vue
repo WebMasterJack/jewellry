f8973f092d25ad8ea9b843d24ba91615a10761ac
s2d1ent <vornfrost@gmail.com>
2022-06-10 11:47:07 +0300

    profile complete 80%

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,74 @@
             <a class="nav-link" href="#">Услуги</a>
             <a class="nav-link" href="#company">О компании</a>
             <a class="nav-link" href="#contact">Контакты</a>
+            <button
+      type="button"
+      class="btn btn-primary h-75 mt-1"
+      data-bs-toggle="modal"
+      data-bs-target="#modal"
+    >
+      Войти
+    </button>
+    <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" v-if="visible">
+      <div class="modal-content bg-dark">
+        <div class="row p-5">
+            <div class="col-12">
+              
+            <input type="name" class="form-control test-1-fps" placeholder="Имя" v-model="name" />
+            <p v-if="visibleError"> {{errors[0].name}}</p>
             
+          </div>
+          <div class="col-12 mt-2">
+            <input type="phone" class="form-control test-1-fpn" placeholder="Телефон" v-model="phone" />
+            <p v-if="visibleError"> {{errors[0].phone}}</p>
+            
+          </div>
+          <div class="col-12 mt-2">
+            <input type="email" class="form-control test-1-fps" placeholder="Почта" v-model="email" />
+            <p v-if="visibleError"> {{errors[0].email}}</p>
+            
+          </div>
+          <div class="col-12 mt-2">
+            <input type="password" class="form-control test-1-fps" placeholder="Пароль" v-model="password" />
+            <p v-if="visibleError"> {{errors[0].password}}</p>
+            
+          </div>
+            
+          <div class="col-12 mt-4">
+            <button class="btn btn-primary w-100 test-1-fbs" @click="register(name,phone,email,password)">Готово</button>
+            <div class="text-center mt-2">Уже зарегистрированы?</div>
+            <a href="#" @click="reglog">Войти</a>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-dialog" v-else>
+      <div class="modal-content bg-dark">
+        <div class="row p-5">
+           
+          <div class="col-12 mt-2">
+            <p v-if="visibleError"> {{errors[0].pp}}</p>
+            <input type="phone" class="form-control test-1-fpn" placeholder="Телефон" v-model="phone"/>
+             <p v-if="visibleError"> {{errors[0].phone}}</p>
+          </div>
+         
+          <div class="col-12 mt-2">
+            <input type="password" class="form-control test-1-fps" placeholder="Пароль" v-model="password"/>
+            <p v-if="visibleError"> {{errors[0].password}}</p>
+          </div>
+
+          <div class="col-12 mt-4">
+            <button class="btn btn-primary w-100 test-1-fbs" @click="login(phone,password)">Готово</button>
+            <div class="text-center mt-2">Еще не зарегистрированы?</div>
+            <a href="#" @click="reglog">Зарегистрироваться</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
           </nav>
         
       </header>
@@ -29,11 +96,95 @@
     
 
 </template>
+<style>
+
+</style>
 <script>
-// import Reference from '@/components/Reference.vue'
+
 export default {
+  data(){
+        return{
+            errors:[],
+            name: '',
+            phone: '',
+            email: '',
+            password: '',
+            visible:true,
+            visibleError:false,
+        }
+        
+    },
     name: 'Header',
-    // components: Reference
+   methods:{
+      reglog(){this.visible= !this.visible;},
+      async register() {
+        this.errors.length = 0;
+            let data = {
+                name: this.name,
+                phone: this.phone,
+                email:this.email,
+                password: this.password,
+            };
+          const response = await fetch("http://localhost:8000/api/register", {
+            method: "POST",
+            body:JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'},
+          });
+          if (response.ok) 
+          { 
+            // если HTTP-статус в диапазоне 200-299
+            let {name,phone,email,password}=this;
+            this.$router.replace({name:'Profile',params:{name,phone,email,password}});
+          }
+          else
+          {
+               let json = await response.json();
+              this.visibleError=!json.ok;
+            
+              this.errors.push(json.error.errors);
+             
+               console.log(this.errors);
+                
+              
+          }
+          console.log(response,this.name,this.phone,this.email,this.password);
+        },
+        async login() {
+           this.errors.length = 0;
+            let data = {
+                
+                phone: this.phone,
+                
+                password: this.password,
+                
+            };
+          const response = await fetch("http://localhost:8000/api/login", 
+          {
+            method: "POST",
+            body:JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'},
+            
+          });
+          if (response.ok) 
+          {
+          const result = await response.json(); // читаем ответ в формате JSON
+          let id=result.data.id;
+          this.$router.push({name:'Profile',params:{id}});
+          }
+          else
+          {
+            let json = await response.json();
+              this.visibleError=!json.ok;
+            
+              this.errors.push(json.error.errors);
+             
+               console.log(this.errors);
+              console.log(123)
+          }
+          // console.log(this.phone,this.password);
+        },
+      },
 
 }
+
 </script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,43 @@
 <template>
 <div class="profile">
-dsadsadas    
+ID: {{this.id}} <br>
+Имя: {{database[0]===undefined?"":database[0].name}}<br> 
+Номер: {{database[0]===undefined?"":database[0].phone}}
 </div>    
 </template>
+<style scoped>
+main h1,main div{
+    display: none;
+}
+</style>
 <script>
 export default {
+    data(){
+        return{
+            database:[],
+            errors:[],
+            id:this.$route.params.id,
+        }
+    },
     name:'Profile',
-    props: ['name','phone','email'],
+    async  mounted()
+ {  let url = `http://localhost:8000/api/profile/${this.id}`;
+      console.log(url)
+        try
+        {
+         
+          let url = `http://localhost:8000/api/profile/${this.id}`;
+          let response = await fetch(url);
+          const result = await response.json(); // читаем ответ в формате JSON
+          this.database.push(result.profile);
+          console.log(result)
+
+        }
+        catch(error)
+        {
+          this.errors.push(error);
+        }
+        
+  },
 }
 </script>
